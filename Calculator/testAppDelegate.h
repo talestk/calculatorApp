bf83e1e48e089860d5f345f42a11f6636924f3dd
Tales Takemiya <talestakemiya@Taless-MacBook-Pro.local>
2013-08-24 10:48:17 -0500

    Initial Commit

---
+++ Calculator/testAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  testAppDelegate.h
+//  Calculator
+//
+//  Created by Tales Takemiya on 8/24/13.
+//  Copyright (c) 2013 Tales Takemiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/testViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewController.h
+//  Calculator
+//
+//  Created by Tales Takemiya on 8/24/13.
+//  Copyright (c) 2013 Tales Takemiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewController : UIViewController
+
+@end
